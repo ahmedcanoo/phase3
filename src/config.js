867c9feaaa9e6908c639b3ca7939fd98514a760a
ahmedcanoo <149202243+ahmedcanoo@users.noreply.github.com>
2024-12-24 22:06:12 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendURL: "http://go-backend-ahmedcano-dev.apps.rm1.0a51.p1.openshiftapps.com",
+    backendURL: "https://back-end5-ahmedcano-dev.apps.rm1.0a51.p1.openshiftapps.com",
   };
   
   export default config;
