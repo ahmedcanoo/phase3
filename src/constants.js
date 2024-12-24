19744ccd59a460d9dac206d2c748aeef53662760
ahmedcanoo <149202243+ahmedcanoo@users.noreply.github.com>
2024-12-24 22:06:48 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://go-backend-ahmedcano-dev.apps.rm1.0a51.p1.openshiftapps.com";
+export const BACKEND_URL = "https://back-end5-ahmedcano-dev.apps.rm1.0a51.p1.openshiftapps.com";
